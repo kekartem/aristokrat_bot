2f55d6c2f468413acebbe00cd9343883ad3bc974
Артем Хакимов <arthack00@gmail.com>
2023-03-31 10:01:18 +0500

    removed waifu

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def start(message: types.Message):
     keyboard = types.InlineKeyboardMarkup(row_width=1).add(types.InlineKeyboardButton('🔧 Конструктор модульной печи', callback_data='action1'), types.InlineKeyboardButton('♨️ Примеры', callback_data='action2'), types.InlineKeyboardButton('📞 Контакты', callback_data='action3'))
     await message.answer('Вас приветствует бот компании "Аристкратъ". Выберите нужное действие:',
                                  reply_markup=keyboard)
-    await message.answer_photo(types.InputFile(requests.get('https://api.waifu.im/search?is_nsfw=true').json()['images'][0]['url']))
+    # await message.answer_photo(types.InputFile(requests.get('https://api.waifu.im/search?is_nsfw=true').json()['images'][0]['url']))
 
 
 
